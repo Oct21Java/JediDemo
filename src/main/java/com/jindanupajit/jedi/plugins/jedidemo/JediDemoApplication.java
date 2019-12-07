c3590597d7d815cc861ad9a0637cd27b0ee62fab
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-12-06 20:12:26 -0500

    Spring Initializer (Web, Security, JPA, H2, MySQL, and ThymeLeaf)

---
+++ src/main/java/com/jindanupajit/jedi/plugins/jedidemo/JediDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jindanupajit.jedi.plugins.jedidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JediDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jindanupajit/jedi/plugins/jedidemo/JediDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jindanupajit.jedi.plugins.jedidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JediDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
